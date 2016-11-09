cf7f86f0b6732d59b4e06d7bd014796cb86e006b
mikebro <Michael.Brothers@trinet.com>
2016-11-08 20:39:23 -0800

    updated TestBoolean for string compare test

--- TestBoolean.java
+++ TestBoolean.java
@@ -4,5 +4,12 @@ public class TestBoolean {
    public static void main(String[] args) {
       boolean var = ( 1 == 2 );
       System.out.println( "var = " + var );
+
+
+      String defaultGroup = "0";
+      boolean boo = defaultGroup.equals( "1" );
+      System.out.println( "flag = " + boo );
+
+      
    }
 }
