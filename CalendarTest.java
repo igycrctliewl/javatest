28218ee32dc40c7fa9dd2a1fc479426a8499eb7b
mikebro <Michael.Brothers@trinet.com>
2016-11-17 17:19:55 -0800

    played with Calendar some more (trying to manipulate time zones)

--- CalendarTest.java
+++ CalendarTest.java
@@ -2,12 +2,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class CalendarTest {
 
    public static void main(String[] args) {
 
-      SimpleDateFormat fmt = new SimpleDateFormat( "MM/dd/yyyy" );
+      SimpleDateFormat fmt = new SimpleDateFormat( "MM/dd/yyyy  hh:mm:ss  z" );
 
       Calendar cal = Calendar.getInstance();   
       cal.clear();
@@ -16,14 +17,20 @@ public class CalendarTest {
       cal.set( Calendar.MONTH, Calendar.OCTOBER );
       cal.set( Calendar.DAY_OF_MONTH, 1 );
       cal.set( Calendar.YEAR, 2016 );
-
+      
       System.out.println( "d2:" + cal + ":\n" );
       System.out.println( "date:" + cal.getTime() + ":\n" );
       System.out.println( "date:" + fmt.format( cal.getTime() ) + ":\n" );
       
+      System.out.println( "________________________________________________\n\n" );
+      
+//germanyTime.setTimeInMillis(localTime.getTimeInMillis());
 
-
-
+      TimeZone tz = TimeZone.getTimeZone("UTC");
+      System.out.println( tz.getID() );
+      cal.setTimeZone( tz );
+      System.out.println( "d3:" + cal + ":\n" );
+      System.out.println( "date:" + fmt.format( cal.getTime() ) + ":\n" );
      
    }
 }
