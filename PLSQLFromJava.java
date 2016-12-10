17e22638b340f51a0a5fa18ab54ae4c22b6d1d26
mikebro <Michael.Brothers@trinet.com>
2016-12-09 16:49:41 -0800

    an unsuccessful attempt to run getNextBenProg from Java

--- PLSQLFromJava.java
+++ PLSQLFromJava.java
@@ -16,21 +16,23 @@ public class PLSQLFromJava {
          Connection vDatabaseConnection = DriverManager.getConnection( "jdbc:oracle:thin:@dbhrlites.trinet.com:1725:hrlites", "sysadm", "mhall510" );
          
          // PREPARE PL/SQL FUNCTION CALL ("?" IS A PLACEHOLDER FOR RETURN VALUES AND PARAMETER VALUES, NUMBERED FROM LEFT TO RIGHT)
-         CallableStatement vStatement = vDatabaseConnection.prepareCall( "begin t2_bss.insertNewAmbBenProgEffdt( ?, ? ); end;" );
+         CallableStatement vStatement = vDatabaseConnection.prepareCall( "begin ? := t2_bss.getNextBenProg(); end;" );
          // USE EITHER THE "BLOCK" SYNTAX ABOVE, OR "ANSI 92" SYNTAX BELOW
          //CallableStatement vStatement = vDatabaseConnection.prepareCall( "{ ? = call javatest( ?, ? ) }" );
          
          // SET INPUT PARAMETERS AND DECLARE TYPES FOR RETURN VALUES
          //  not needed for this statement   ----vStatement.registerOutParameter( 1, Types.VARCHAR );   // declare type of function return value "begin ? ..."
-         vStatement.setString( 1, "0016NX" );                  // set value of first function parameter "... javatest( ?, ..."
-         vStatement.setDate( 2, java.sql.Date.valueOf( "2016-12-01" ) );                  // set value of second function parameter 
+         //vStatement.setString( 1, "0016NX" );                  // set value of first function parameter "... javatest( ?, ..."
+         //vStatement.setDate( 2, java.sql.Date.valueOf( "2016-12-01" ) );                  // set value of second function parameter 
    
-   
-         //  not needed for this statement   ----vStatement.registerOutParameter( 3, Types.VARCHAR );   // declare type of second function parameter "..., ? ); end;"
+         // initialize variable #1
+         vStatement.setString( 1, "      " );                  // set value of first function parameter "... javatest( ?, ..."
          
          // CALL THE PL/SQL FUNCTION
          vStatement.execute();
          
+         String newBenProg = vStatement.getString( 1 );
+         System.out.println( "New benefit program:" + newBenProg );
          
          // CLOSE THE DATABASE CONNECTION
          vDatabaseConnection.close();
