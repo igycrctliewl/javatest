92949bfc2864a739bf304a240ab55e169123113a
mikebro <Michael.Brothers@trinet.com>
2017-05-10 17:34:38 -0700

    additional array tests

--- TestArray.java
+++ TestArray.java
@@ -2,10 +2,11 @@
 public class TestArray {
    public static void main(String[] args) {
       String[] list =  new String[3];
+      System.out.println( "list size: " + list.length );
 
       list[0] = "23";
       list[1] = "30";
-      list[2] = "31";
+      //list[2] = "31";
 
       for( String planType : list ) {
          System.out.println( "===>  " + planType );
