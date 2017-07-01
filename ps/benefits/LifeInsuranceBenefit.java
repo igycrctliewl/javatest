cb49ed15b792ddbbfcc977e3a23855114ba3bcbf
sonyVaio <yomikebro@gmail.com>
2017-07-01 14:59:35 -0700

    introduced shell classes for rate and coverage formula

--- ps/benefits/LifeInsuranceBenefit.java
+++ ps/benefits/LifeInsuranceBenefit.java
@@ -2,6 +2,8 @@ package ps.benefits;
 
 public class LifeInsuranceBenefit extends Benefit {
 
+   
+
    public LifeInsuranceBenefit( String planType, String benefitPlan ) {
       super.planType = planType;
       super.benefitPlan = benefitPlan;
--- ps/test/PsJobTest.java
+++ ps/test/PsJobTest.java
@@ -3,14 +3,29 @@ package ps.test;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import ps.benefits.BenefitRate;
+import ps.benefits.CoverageFormula;
 import ps.benefits.LifeInsuranceBenefit;
 import ps.hcm.PsJob;
 
 /* unit test PsJob class  */
 
 public class PsJobTest {
-   
+
    public static void main(String[] args) {
+      mainBenefits( args );
+   }
+   
+   public static void mainBenefits(String[] args) {
+      BenefitRate rt = new BenefitRate( "0ORPLF" );
+      CoverageFormula cvg = new CoverageFormula( "AMB0000123" );
+      LifeInsuranceBenefit life = new LifeInsuranceBenefit( "23", "000TMB" );
+      System.out.println( "rate: " + rt.toString() );
+      System.out.println( "cvrg: " + cvg.toString() );
+      System.out.println( "life: " + life.toString() );
+   }
+   
+   public static void mainJob(String[] args) {
       List<PsJob> jobRecords = new ArrayList<PsJob>();
 
       jobRecords.add( new PsJob( new BigDecimal( "320000" ), new BigDecimal( "305000" ) ) );
@@ -20,14 +35,11 @@ public class PsJobTest {
       jobRecords.add( new PsJob( new BigDecimal( "82600" ), new BigDecimal( "75500" ) ) );
       jobRecords.add( new PsJob( new BigDecimal( "60000" ), BigDecimal.ZERO ) );
 
-      for( PsJob j : jobRecords ) {      
+      for( PsJob j : jobRecords ) {
          System.out.println( j.toString() );
          System.out.println( "     " + j.getAnnualRate() );
          System.out.println( "     " + j.getAbbr() );
       }
-      
-      LifeInsuranceBenefit life = new LifeInsuranceBenefit( "23", "000TMB" );
-      System.out.println( "life: " + life.toString() );
    }
-  
+
 }
