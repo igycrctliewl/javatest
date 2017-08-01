e6b2745ad3691cf07145a3f401a5d56b3427e51d
sonyVaio <yomikebro@gmail.com>
2017-07-31 22:06:13 -0700

    introduced new java.time package and tested some new classes

--- SimpleDateProject/src/mb/test/DateTest.java
+++ SimpleDateProject/src/mb/test/DateTest.java
@@ -1,5 +1,8 @@
 package mb.test;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -13,23 +16,35 @@ public class DateTest {
    public static void main( String[] args ) {
       Date date = new Date();
       System.out.println( date );
-      
+
       //String[] tz = TimeZone.getAvailableIDs();
       //for( String s : tz ) {
       //   System.out.println( s );
       //}
-      
+
       Calendar instant = Calendar.getInstance( TimeZone.getTimeZone("UTC"), Locale.UK );
-      
+
       instant.set( Calendar.HOUR, 0 );
       instant.set( Calendar.HOUR_OF_DAY, 0 );
       instant.set( Calendar.MINUTE, 0 );
       instant.set( Calendar.SECOND, 0 );
       instant.set( Calendar.MILLISECOND, 0 );
 
-      
+
       System.out.println( instant );
       System.out.println( instant.getTime() );
 
+
+      LocalDate today = LocalDate.now();
+      System.out.println( today );
+      LocalDate dob = LocalDate.of( 1966, Month.JANUARY, 3 );
+      System.out.println( dob );
+      long days = dob.until( today, ChronoUnit.DAYS );
+      System.out.println( days );
+
+      dob = LocalDate.of( 1997, Month.NOVEMBER, 10 );
+      System.out.println( dob );
+      days = dob.until( today, ChronoUnit.DAYS );
+      System.out.println( days );
    }
 }
--- SimpleDateProject/src/mb/test/TestDate.java
+++ SimpleDateProject/src/mb/test/TestDate.java
@@ -1,7 +1,6 @@
 package mb.test;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Date;
 
 public class TestDate {
