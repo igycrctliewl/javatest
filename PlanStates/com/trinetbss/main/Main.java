c075db413b2deb377c54c17296edd41889ff6706
mikebro <Michael.Brothers@trinet.com>
2017-09-15 16:22:29 -0700

    Changed Main class to use a URL and an input parameter instead of a hard-coded JSON stream

--- PlanStates/com/trinetbss/main/Main.java
+++ PlanStates/com/trinetbss/main/Main.java
@@ -5,31 +5,39 @@ import com.trinetbss.json.geocode.Geocode;
 import com.trinetbss.json.geocode.Result;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.net.URL;
+import java.net.MalformedURLException;
 
 public class Main {
 	public static void main(String[] args) {
 
-		String json = "{ \"results\" : [ { \"address_components\" : [ { \"long_name\" : \"92101\", \"short_name\" : \"92101\", \"types\" : [ \"postal_code\" ] }, { \"long_name\" : \"San Diego\", \"short_name\" : \"San Diego\", \"types\" : [ \"locality\", \"political\" ] }, { \"long_name\" : \"San Diego County\", \"short_name\" : \"San Diego County\", \"types\" : [ \"administrative_area_level_2\", \"political\" ] }, { \"long_name\" : \"California\", \"short_name\" : \"CA\", \"types\" : [ \"administrative_area_level_1\", \"political\" ] }, { \"long_name\" : \"United States\", \"short_name\" : \"US\", \"types\" : [ \"country\", \"political\" ] } ], \"formatted_address\" : \"San Diego, CA 92101, USA\", \"geometry\" : { \"bounds\" : { \"northeast\" : { \"lat\" : 32.742536, \"lng\" : -117.145741 }, \"southwest\" : { \"lat\" : 32.6943429, \"lng\" : -117.2165131 } }, \"location\" : { \"lat\" : 32.7269669, \"lng\" : -117.1647094 }, \"location_type\" : \"APPROXIMATE\", \"viewport\" : { \"northeast\" : { \"lat\" : 32.742536, \"lng\" : -117.145741 }, \"southwest\" : { \"lat\" : 32.6943429, \"lng\" : -117.2165131 } } }, \"place_id\" : \"ChIJK-YIt0JT2YARI5JWWz1ORQk\", \"types\" : [ \"postal_code\" ] } ], \"status\" : \"OK\" }";
-		System.out.println(json);
+		//String json = "{ \"results\" : [ { \"address_components\" : [ { \"long_name\" : \"92101\", \"short_name\" : \"92101\", \"types\" : [ \"postal_code\" ] }, { \"long_name\" : \"San Diego\", \"short_name\" : \"San Diego\", \"types\" : [ \"locality\", \"political\" ] }, { \"long_name\" : \"San Diego County\", \"short_name\" : \"San Diego County\", \"types\" : [ \"administrative_area_level_2\", \"political\" ] }, { \"long_name\" : \"California\", \"short_name\" : \"CA\", \"types\" : [ \"administrative_area_level_1\", \"political\" ] }, { \"long_name\" : \"United States\", \"short_name\" : \"US\", \"types\" : [ \"country\", \"political\" ] } ], \"formatted_address\" : \"San Diego, CA 92101, USA\", \"geometry\" : { \"bounds\" : { \"northeast\" : { \"lat\" : 32.742536, \"lng\" : -117.145741 }, \"southwest\" : { \"lat\" : 32.6943429, \"lng\" : -117.2165131 } }, \"location\" : { \"lat\" : 32.7269669, \"lng\" : -117.1647094 }, \"location_type\" : \"APPROXIMATE\", \"viewport\" : { \"northeast\" : { \"lat\" : 32.742536, \"lng\" : -117.145741 }, \"southwest\" : { \"lat\" : 32.6943429, \"lng\" : -117.2165131 } } }, \"place_id\" : \"ChIJK-YIt0JT2YARI5JWWz1ORQk\", \"types\" : [ \"postal_code\" ] } ], \"status\" : \"OK\" }";
+		//System.out.println(json);
+      System.out.println("class test:" + System.out.getClass().getName() );
 
-		ObjectMapper mapper = new ObjectMapper();
-		Geocode obj = null;
-		try {
-			obj = mapper.readValue(json, Geocode.class);
-		} catch (IOException e) {
-			System.out.println("caught IOException");
-			e.printStackTrace();
-		}
-		System.out.println(obj);
+      try {
+         URL geogApi = new URL( "http://maps.googleapis.com/maps/api/geocode/json?address=" + args[0] );
 
-		for (Result r : obj.getResults()) {
-			System.out.println(r.getFormattedAddress());
-			for (AddressComponent ac : r.getAddressComponents()) {
-				System.out.println("ac.types[0]:" + ac.getTypes().get(0));
-				if ("administrative_area_level_1".equals(ac.getTypes().get(0))) {
-					System.out.println("State:" + ac.getShortName());
-				}
-			}
-		}
+         ObjectMapper mapper = new ObjectMapper();
+         Geocode obj = null;
+         obj = mapper.readValue( geogApi, Geocode.class);
+         System.out.println(obj);
+
+         for (Result r : obj.getResults()) {
+            System.out.println(r.getFormattedAddress());
+            for (AddressComponent ac : r.getAddressComponents()) {
+               System.out.println("ac.types[0]:" + ac.getTypes().get(0));
+               if ("administrative_area_level_1".equals(ac.getTypes().get(0))) {
+                  System.out.println("State:" + ac.getShortName());
+               }
+            }
+         }
+      } catch ( MalformedURLException mal ) {
+         System.out.println("bad url");
+         mal.printStackTrace();
+      } catch ( IOException e ) {
+         System.out.println("caught IOException:" + e.getClass().getName() );
+         e.printStackTrace();
+      }
 	}
 }
