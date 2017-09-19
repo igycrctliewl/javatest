3b2566bd554579b187be61a81da4b31017c5054b
mikebro <Michael.Brothers@trinet.com>
2017-09-18 19:02:37 -0700

    reconfigured PlansByState as an instantiable object

--- PlanStates/com/trinetbss/main/Main.java
+++ PlanStates/com/trinetbss/main/Main.java
@@ -3,6 +3,7 @@ package com.trinetbss.main;
 import com.trinetbss.json.geocode.AddressComponent;
 import com.trinetbss.json.geocode.Geocode;
 import com.trinetbss.json.geocode.Result;
+import com.trinetbss.sql.PlansByState;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URL;
@@ -13,6 +14,11 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
+      System.out.println( "Main.main()" );
+      PlansByState.runQuery( "108", "01-OCT-2017" );
+   }
+
+	public static void main1(String[] args) {
 
       // hard-coded test
 		//String json = "{ \"results\" : [ { \"address_components\" : [ { \"long_name\" : \"92101\", \"short_name\" : \"92101\", \"types\" : [ \"postal_code\" ] }, { \"long_name\" : \"San Diego\", \"short_name\" : \"San Diego\", \"types\" : [ \"locality\", \"political\" ] }, { \"long_name\" : \"San Diego County\", \"short_name\" : \"San Diego County\", \"types\" : [ \"administrative_area_level_2\", \"political\" ] }, { \"long_name\" : \"California\", \"short_name\" : \"CA\", \"types\" : [ \"administrative_area_level_1\", \"political\" ] }, { \"long_name\" : \"United States\", \"short_name\" : \"US\", \"types\" : [ \"country\", \"political\" ] } ], \"formatted_address\" : \"San Diego, CA 92101, USA\", \"geometry\" : { \"bounds\" : { \"northeast\" : { \"lat\" : 32.742536, \"lng\" : -117.145741 }, \"southwest\" : { \"lat\" : 32.6943429, \"lng\" : -117.2165131 } }, \"location\" : { \"lat\" : 32.7269669, \"lng\" : -117.1647094 }, \"location_type\" : \"APPROXIMATE\", \"viewport\" : { \"northeast\" : { \"lat\" : 32.742536, \"lng\" : -117.145741 }, \"southwest\" : { \"lat\" : 32.6943429, \"lng\" : -117.2165131 } } }, \"place_id\" : \"ChIJK-YIt0JT2YARI5JWWz1ORQk\", \"types\" : [ \"postal_code\" ] } ], \"status\" : \"OK\" }";
--- PlanStates/com/trinetbss/sql/PlansByState.java
+++ PlanStates/com/trinetbss/sql/PlansByState.java
@@ -1,3 +1,5 @@
+package com.trinetbss.sql;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,65 +7,100 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PlansByState {
-   public static void main( String[] args ) {
+	public static void main( String[] args ) {
+		System.out.println( "main method" );
+		PlansByState pbs = new PlansByState();
+		pbs.runQuery( "108", "01-OCT-2017" );
 
-      try {
+		System.out.println( "display results in caller" );
 
-         // Initialize and connect
-         DriverManager.registerDriver( new oracle.jdbc.driver.OracleDriver() );
-         Connection vDatabaseConnection = DriverManager.getConnection( "jdbc:oracle:thin:@dbhrlites.trinet.com:1725:hrlites", "sysadm", "mhall510" );
+		try {
+			while( pbs.queryResult.next() ) {
+				String planType = pbs.queryResult.getString( "PLAN_TYPE" );
+				String benefitPlan = pbs.queryResult.getString( "BENEFIT_PLAN" );
+				String planName = pbs.queryResult.getString( "PLAN_NAME" );
+				String geoLoc = pbs.queryResult.getString( "LOCATION_TBL_ID" );
 
-         String getPlansSQL =
-              "SELECT DISTINCT PLAN_TYPE " +
-              "              , BENEFIT_PLAN " +
-              "              , ( SELECT BP.DESCR " +
-              "                    FROM PS_BENEF_PLAN_TBL BP " +
-              "                   WHERE BP.PLAN_TYPE = OP.PLAN_TYPE " +
-              "                     AND BP.BENEFIT_PLAN = OP.BENEFIT_PLAN " +
-              "                     AND BP.EFFDT = (SELECT MAX( EFFDT ) " +
-              "                                       FROM PS_BENEF_PLAN_TBL B1 " +
-              "                                      WHERE B1.PLAN_TYPE = BP.PLAN_TYPE " +
-              "                                        AND B1.BENEFIT_PLAN = BP.BENEFIT_PLAN " +
-              "                                        AND B1.EFFDT <= OP.EFFDT) ) " +
-              "                   AS PLAN_NAME " +
-              "              , LOCATION_TBL_ID " +
-              "  FROM PS_BEN_DEFN_OPTN OP " +
-              " WHERE BENEFIT_PROGRAM = '101' " +
-              "   AND EFFDT = DATE '2018-01-01' " +
-              "   AND PLAN_TYPE = '10' " +
-              "   AND OPTION_TYPE = 'O' " +
-              "   AND NOT ( ELIG_RULES_ID IN ('2009', '23GC', '236Q') ) " +
-              "   AND NOT ( LOCATION_TBL_ID = ' ' )";
+				System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
+			}
+		} catch( SQLException e ) {
+			System.out.println( e.toString() );
+		}
 
-         // Prepare statement from SQL string
-         PreparedStatement sqlStmt = vDatabaseConnection.prepareStatement( getPlansSQL );
+		pbs.close();
 
-         // Set values for parameters
-         //sqlStmt.setLong( 1, 2 );                                         // realmID
-         //sqlStmt.setDate( 2, java.sql.Date.valueOf( "2017-01-01" ) );     // effdt
-         //sqlStmt.setLong( 3, 2 );                                         // realmID
-         //sqlStmt.setDate( 4, java.sql.Date.valueOf( "2017-01-01" ) );     // effdt
-         //sqlStmt.setString( 5, "Q1" );                                    // OE quarter
-
-         // run the query
-         ResultSet qResult = sqlStmt.executeQuery();
-
-         while( qResult.next() ) {
-       	   String planType = qResult.getString( "PLAN_TYPE" );
-      	   String benefitPlan = qResult.getString( "BENEFIT_PLAN" );
-     	      String planName = qResult.getString( "PLAN_NAME" );
-            String geoLoc = qResult.getString( "LOCATION_TBL_ID" );
-
-            System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
-         }
+	}
 
 
-         // CLOSE THE DATABASE CONNECTION
-         vDatabaseConnection.close();
+	private Connection vDatabaseConnection;
+	public ResultSet queryResult;
 
-      } catch( SQLException e ) {
-         System.out.println( e.toString() );
-      }
+	public PlansByState() {
+		
+	}
 
-   }
+	public void runQuery( String benefitProgram, String effdtStr ) {
+		System.out.println( "PlansByState.runQuery( " + benefitProgram + ", " + effdtStr + " )" );
+		try {
+
+			// Initialize and connect
+			DriverManager.registerDriver( new oracle.jdbc.driver.OracleDriver() );
+			vDatabaseConnection = DriverManager.getConnection( "jdbc:oracle:thin:@dbhrlites.trinet.com:1725:hrlites", "sysadm", "mhall510" );
+
+			String getPlansSQL =
+				  "SELECT DISTINCT PLAN_TYPE " +
+				  "              , BENEFIT_PLAN " +
+				  "              , ( SELECT BP.DESCR " +
+				  "                    FROM PS_BENEF_PLAN_TBL BP " +
+				  "                   WHERE BP.PLAN_TYPE = OP.PLAN_TYPE " +
+				  "                     AND BP.BENEFIT_PLAN = OP.BENEFIT_PLAN " +
+				  "                     AND BP.EFFDT = (SELECT MAX( EFFDT ) " +
+				  "                                       FROM PS_BENEF_PLAN_TBL B1 " +
+				  "                                      WHERE B1.PLAN_TYPE = BP.PLAN_TYPE " +
+				  "                                        AND B1.BENEFIT_PLAN = BP.BENEFIT_PLAN " +
+				  "                                        AND B1.EFFDT <= OP.EFFDT) ) " +
+				  "                   AS PLAN_NAME " +
+				  "              , LOCATION_TBL_ID " +
+				  "  FROM PS_BEN_DEFN_OPTN OP " +
+				  " WHERE BENEFIT_PROGRAM = ? " +
+				  "   AND EFFDT = TO_DATE( ?, 'DD-MON-YYYY' ) " +
+				  "   AND PLAN_TYPE = '10' " +
+				  "   AND OPTION_TYPE = 'O' " +
+				  "   AND NOT ( ELIG_RULES_ID IN ('2009', '23GC', '236Q') ) " +
+				  "   AND NOT ( LOCATION_TBL_ID = ' ' )";
+
+			// Prepare statement from SQL string
+			PreparedStatement sqlStmt = vDatabaseConnection.prepareStatement( getPlansSQL );
+
+			// Set values for parameters
+			sqlStmt.setString( 1, benefitProgram );
+			sqlStmt.setString( 2, effdtStr );
+
+			// run the query
+			queryResult = sqlStmt.executeQuery();
+
+/*       while( qResult.next() ) {
+				String planType = qResult.getString( "PLAN_TYPE" );
+			   String benefitPlan = qResult.getString( "BENEFIT_PLAN" );
+				String planName = qResult.getString( "PLAN_NAME" );
+				String geoLoc = qResult.getString( "LOCATION_TBL_ID" );
+
+				System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
+			}  */
+
+		} catch( SQLException e ) {
+			System.out.println( e.toString() );
+		}
+	}
+
+	
+	public void close() {
+		try {
+			System.out.println( "close the database connection" );
+			vDatabaseConnection.close();
+		} catch( SQLException e ) {
+			System.out.println( e.toString() );
+		}
+
+	}
 }
