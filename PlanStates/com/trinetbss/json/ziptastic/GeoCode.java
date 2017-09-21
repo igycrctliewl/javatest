f0acf22ba36101158b593e972ed6b36d9f0e6ff5
mikebro <Michael.Brothers@trinet.com>
2017-09-21 13:51:14 -0700

    added a new json parser class for a different public API ziptastic

---
+++ PlanStates/com/trinetbss/json/ziptastic/GeoCode.java
@@ -0,0 +1,67 @@
+//-----------------------------------com.ziptastic.GeoCode.java-----------------------------------
+
+package com.ziptastic;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "country", "state", "city" })
+public class GeoCode {
+
+	@JsonProperty("country")
+	private String country;
+	@JsonProperty("state")
+	private String state;
+	@JsonProperty("city")
+	private String city;
+	@JsonIgnore
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	@JsonProperty("country")
+	public String getCountry() {
+		return country;
+	}
+
+	@JsonProperty("country")
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@JsonProperty("state")
+	public String getState() {
+		return state;
+	}
+
+	@JsonProperty("state")
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@JsonProperty("city")
+	public String getCity() {
+		return city;
+	}
+
+	@JsonProperty("city")
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@JsonAnyGetter
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	@JsonAnySetter
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
+
+}
--- PlanStates/com/trinetbss/main/Main.java
+++ PlanStates/com/trinetbss/main/Main.java
@@ -21,15 +21,15 @@ import java.util.Map;
 
 public class Main {
 	public static void main( String[] args ) {
-      System.out.println( "Main.main()" );
+		System.out.println( "Main.main()" );
 
 		String benefitProgram = "108";
 		String effdtStr = "01-OCT-2017";
 
-      Map< String, PSGeogLocn > geoLocationStates =  new HashMap<>();
+		Map< String, PSGeogLocn > geoLocationStates =  new HashMap<>();
 		PlansLocations pbs = new PlansLocations();
 
-      pbs.runQuery( benefitProgram, effdtStr );
+		pbs.runQuery( benefitProgram, effdtStr );
 		try {
 			while( pbs.queryResult.next() ) {
 				String planType = pbs.queryResult.getString( "PLAN_TYPE" );
@@ -38,25 +38,25 @@ public class Main {
 				String geoLoc = pbs.queryResult.getString( "LOCATION_TBL_ID" );
 
 				if( !geoLocationStates.containsKey( geoLoc ) ) {
-					System.out.println( "********** getting geo location definition **********" );
+					//System.out.println( "********** getting geo location definition **********" );
 					PSGeogLocn psGeogLocn = new PSGeogLocn( geoLoc, effdtStr );
-					System.out.println( "********** get geo location ranges **********" );
+					//System.out.println( "********** get geo location ranges **********" );
 					Main.getGeogLocnRangeStates( psGeogLocn );
 					System.out.println( " -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* states so far: " + psGeogLocn.states );
 					geoLocationStates.put( geoLoc, psGeogLocn );
- 				}
+				}
 
 				System.out.println( "********** associate plan with states **********" );
- 				System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
+				System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
 			}
 		} catch( SQLException e ) {
 			System.out.println( e.toString() );
 		}
 
-		System.out.println( "Geo location map contained " + geoLocationStates.size() + " entries." );
+		//System.out.println( "Geo location map contained " + geoLocationStates.size() + " entries." );
 
 		PSConnect.getInstance().close();
-   }
+	}
 
 	
 	private static void getGeogLocnRangeStates( PSGeogLocn psGeogLocn ) {
@@ -67,12 +67,12 @@ public class Main {
 				String fromZip = ranges.queryResult.getString( "LOCN_FROM" );
 				String toZip = ranges.queryResult.getString( "LOCN_TO" );
 
-				System.out.println( "=====>" + fromZip + " <=> " + toZip + " <=" );
+				//System.out.println( "=====>" + fromZip + " <=> " + toZip + " <=" );
 
 				int fromInt = Integer.parseInt( fromZip );
 				int toInt = Integer.parseInt( toZip.substring( 0, 5 ) );
 				for( int a = fromInt; a <= toInt; a++ ) {
-					System.out.println( "     a:" + a );
+					//System.out.println( "     a:" + a );
 					String zipCode = String.valueOf( a );
 					while( zipCode.length() < 5 ) {
 						zipCode = "0".concat( zipCode );
@@ -91,74 +91,75 @@ public class Main {
 	 */
 	private static String callGeoApi( String zipCode ) {
 
-      Address addr = new Address();
+		Address addr = new Address();
 
-      try {
-         URL geogApi = new URL( "http://maps.googleapis.com/maps/api/geocode/json?address=US&components=postal_code:" + zipCode );
+		try {
+			URL geogApi = new URL( "http://maps.googleapis.com/maps/api/geocode/json?address=US&components=postal_code:" + zipCode );
 
-         ObjectMapper mapper = new ObjectMapper();
-         Geocode obj = null;
-         obj = mapper.readValue( geogApi, Geocode.class);
-         System.out.println(obj);
+			ObjectMapper mapper = new ObjectMapper();
+			Geocode obj = null;
+			obj = mapper.readValue( geogApi, Geocode.class );
+			System.out.println(obj);
 
-         for (Result r : obj.getResults()) {
-            System.out.println(r.getFormattedAddress());
-            for (AddressComponent ac : r.getAddressComponents()) {
-               parseAC( ac, addr );
-            }
-         }
-      } catch ( MalformedURLException mal ) {
-         System.out.println("bad url");
-         mal.printStackTrace();
-      } catch ( IOException e ) {
-         System.out.println("caught IOException:" + e.getClass().getName() );
-         e.printStackTrace();
-      }
+			for (Result r : obj.getResults()) {
+				System.out.println(r.getFormattedAddress());
+				for (AddressComponent ac : r.getAddressComponents()) {
+					parseAC( ac, addr );
+				}
+				System.out.println( "     " + addr );
+			}
+		} catch ( MalformedURLException mal ) {
+			System.out.println("bad url");
+			mal.printStackTrace();
+		} catch ( IOException e ) {
+			System.out.println("caught IOException:" + e.getClass().getName() );
+			e.printStackTrace();
+		}
 
-      if( "US".equals( addr.country ) ) {
-         System.out.println( "Determined state is " + addr.state );
+		if( "US".equals( addr.country ) ) {
+			//System.out.println( "Determined state is " + addr.state );
 			return addr.state;
-      } else {
+		} else {
+			System.out.println( "zipCode " + zipCode + " returned no state value; addr.country = " + addr.country );
 			return "";
 		}
 	}
 
-   private static void parseAC( AddressComponent adr, Address a ) {
-      String longName = adr.getLongName();
-      String shortName = adr.getShortName();
-      List<String> types = adr.getTypes();
+	private static void parseAC( AddressComponent adr, Address a ) {
+		String longName = adr.getLongName();
+		String shortName = adr.getShortName();
+		List<String> types = adr.getTypes();
 
-      System.out.println( ">>>> parseAC" );
-      //System.out.println( "     longName:" + longName );
-      //System.out.println( "     shortName:" + shortName );
+		System.out.println( ">>>> parseAC" );
+		System.out.println( "     longName:" + longName );
+		System.out.println( "     shortName:" + shortName );
 
-      if( types.contains( "postal_code" ) ) {
-         System.out.println( "     zip code is " + shortName );
-         a.zip = shortName;
-      } else if( types.contains( "locality" ) ) {
-         System.out.println( "     city is " + shortName );
-         a.city = shortName;
-      } else if( types.contains( "administrative_area_level_2" ) ) {
-         System.out.println( "     county is " + shortName );
-      } else if( types.contains( "administrative_area_level_1" ) ) {
-         System.out.println( "     state is " + shortName );
-         a.state = shortName;
-      } else if( types.contains( "country" ) ) {
-         System.out.println( "     country is " + shortName );
-         a.country = shortName;
-      }
+		if( types.contains( "postal_code" ) ) {
+			System.out.println( "     zip code is " + shortName );
+			a.zip = shortName;
+		} else if( types.contains( "locality" ) ) {
+			System.out.println( "     city is " + shortName );
+			a.city = shortName;
+		} else if( types.contains( "administrative_area_level_2" ) ) {
+			System.out.println( "     county is " + shortName );
+		} else if( types.contains( "administrative_area_level_1" ) ) {
+			System.out.println( "     state is " + shortName );
+			a.state = shortName;
+		} else if( types.contains( "country" ) ) {
+			System.out.println( "     country is " + shortName );
+			a.country = shortName;
+		}
 
-      System.out.println( "     " + a );
-   }
+	}
 
-   static class Address {
-      String city;
-      String state;
-      String zip;
-      String country;
+	static class Address {
+		String city;
+		String state;
+		String zip;
+		String country;
 
-      public String toString() {
-         return this.getClass().getName() + ":" + this.city + ", " + this.state + "   " + this.zip + "  " + this.country;
-      }
-   }
+		public String toString() {
+			return this.getClass().getName() + ":" + this.city + ", " + this.state + "   " + this.zip + "  " + this.country;
+		}
+	}
 }
