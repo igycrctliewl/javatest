ba273a6eddbc9bbc55ee5f36ef6c74943f7159c2
sonyVaio <yomikebro@gmail.com>
2017-09-23 12:10:15 -0700

    added looping mechanism and progress indicator; FINISHED AND READY FOR THE BIG DOWNLOAD!

--- CarTalk.java
+++ CarTalk.java
@@ -8,47 +8,49 @@ import java.util.regex.Pattern;
 
 
 public class CarTalk {
+	public static final int ONEMEG = 1048576;
 	public static void main( String[] args ) throws Exception {
-
-		URL test = new URL( "https://play.podtrac.com/npr-510208/npr.mc.tritondigital.com/NPR_510208/media/anon.npr-podcasts/podcast/510208/328869101/npr_328869101.mp3?orgId=1&amp;p=510208&amp;story=328869101&amp;t=podcast&amp;e=328869101&amp;siteplayer=true&amp;dl=1" );
-		System.out.println( test.getFile() );
-
-		String path = test.getPath();
-		String localFileName = path.substring( path.lastIndexOf( "/" ) + 1, path.length() );
-		System.out.println( localFileName );
-
-		byte[] bStream = new byte[1048576];
-		InputStream input = test.openStream();
-		FileOutputStream output = new FileOutputStream( localFileName );
-		for( int readBytes = input.read( bStream ); readBytes > 0; readBytes = input.read( bStream ) ) {
-			output.write( bStream, 0, readBytes );
-		}
-
-		input.close();
-		output.close();
-		
-		//lineInputTest();
-
+		downloadAll();
 	}
 
 
-	public static void lineInputTest() throws Exception {
-		System.out.println( ">>>> CarTalk.lineInputTest" );
+	public static void downloadAll() throws Exception {
+		System.out.println( ">>>> CarTalk.downloadAll" );
 
 		BufferedReader br = new BufferedReader( new FileReader( "C:\\Users\\mikebro\\Documents\\cartalk\\AllEpisodes.txt" ) );
+		String outputPath = "Z:\\Shared Music\\CarTalk\\";
 
 		int i = 0;
 		for( String dataLine = br.readLine(); dataLine != null; dataLine = br.readLine() ) {
 			//System.out.println( i++ + " : " + dataLine );
 			String podcast = testRegex( dataLine );
 			if( ! "".equals( podcast ) ) {
-				System.out.println( ">" + podcast + "<" );
+				//System.out.println( ">" + podcast + "<" );
 				URL url = new URL( podcast );
 				String path = url.getPath();
 				String localFileName = path.substring( path.lastIndexOf( "/" ) + 1, path.length() );
-				System.out.println( localFileName );
+				System.out.print( "Downloading " + localFileName );
+				
+				//download episode to local file
+				byte[] bStream = new byte[ONEMEG];
+				int progress = 0;
+				InputStream input = url.openStream();
+				FileOutputStream output = new FileOutputStream( outputPath + localFileName );
+				for( int readBytes = input.read( bStream ); readBytes > 0; readBytes = input.read( bStream ) ) {
+					progress += readBytes;
+					if( progress > ONEMEG ) {
+						System.out.print( "." );
+						progress = 0;
+					}
+					output.write( bStream, 0, readBytes );
+				}
+		
+				input.close();
+				output.close();
+				
+				
+				System.out.println( "done" );
 			}
-			//System.out.println( "URL.getFile():" + url.getFile() );
 		}
 
 		br.close();
@@ -68,4 +70,28 @@ public class CarTalk {
 		}
 	}
 
+	
+	private static void testDownload() throws Exception {
+		URL test = new URL( "https://play.podtrac.com/npr-510208/npr.mc.tritondigital.com/NPR_510208/media/anon.npr-podcasts/podcast/510208/328869101/npr_328869101.mp3?orgId=1&amp;p=510208&amp;story=328869101&amp;t=podcast&amp;e=328869101&amp;siteplayer=true&amp;dl=1" );
+		System.out.println( test.getFile() );
+
+		String path = test.getPath();
+		String localFileName = path.substring( path.lastIndexOf( "/" ) + 1, path.length() );
+		System.out.println( localFileName );
+
+		byte[] bStream = new byte[1048576];
+		InputStream input = test.openStream();
+		FileOutputStream output = new FileOutputStream( localFileName );
+		System.out.print( "Downloading..." );
+		for( int readBytes = input.read( bStream ); readBytes > 0; readBytes = input.read( bStream ) ) {
+			output.write( bStream, 0, readBytes );
+		}
+		System.out.println( "done." );
+
+		input.close();
+		output.close();
+
+	}
+	
+	
 }
