b843fe195358ff25e6d898e24d8ea24507568b66
mikebro <michael.brothers@trinet.com>
2017-11-07 18:06:32 -0800

    imported Password class from an abandoned commit

---
+++ PlanStates/com/trinetbss/main/Password.java
@@ -0,0 +1,40 @@
+package com.trinetbss.main;
+
+import java.io.Console;
+
+public class Password {
+
+	/**
+	 * Uses a standard prompt to get a password without echoing input
+	 */
+	public static String getPassword() throws Exception {
+
+		Console console = System.console();
+		if (console == null) {
+			throw new Exception( "Couldn't get Console instance" );
+		}
+
+		char passwordArray[] = console.readPassword( "Enter your password: " );
+		return new String( passwordArray );
+	}
+
+
+	/**
+	 * Uses a user-supplied prompt to get a password without echoing input
+	 */
+	public static String getPassword( String prompt ) throws Exception {
+
+		Console console = System.console();
+		if (console == null) {
+			throw new Exception( "Couldn't get Console instance" );
+		}
+
+		char passwordArray[] = console.readPassword( prompt );
+		return new String( passwordArray );
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		System.out.println( "Password is " + Password.getPassword( "Enter your password for HRLITES: " ) );
+	}
+}
