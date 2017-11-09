aa1f013ca77961a0ad1235997a43d55dc58cda5a
mikebro <Michael.Brothers@trinet.com>
2017-11-09 15:30:40 -0800

    added include/exclude logic to PSEligRule service 	PSEligRule.states will always contain the list of eligible states even if the PS table lists ineligible states

--- PlanStates/com/trinetbss/main/PSEligRule.java
+++ PlanStates/com/trinetbss/main/PSEligRule.java
@@ -1,9 +1,12 @@
 package com.trinetbss.main;
 
+import com.trinetbss.main.Constants;
 import com.trinetbss.sql.BasEligRules;
 import com.trinetbss.sql.BasEligState;
 import java.sql.Date;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class PSEligRule {
 
@@ -11,7 +14,7 @@ public class PSEligRule {
 	public String effdtStr;
 	public boolean eligUseState;
 	public String eligFlagState;
-	public HashSet<String> states;
+	public Set<String> states;
 
 	public PSEligRule( String eligRulesId, String effdtStr ) {
 		BasEligRules rules = new BasEligRules();
@@ -30,13 +33,26 @@ public class PSEligRule {
 		}
 
 		if( this.eligUseState ) {
-			this.states = new HashSet<>();
+			// Depending on the eligFlagState value, the state list is Eligible or Inelibible states.
+			// If Ineligible, create a set of all states (ineligible ones will be removed).
+			// If Eligible, create an empty set (eligible states will be added).
+			if( "I".equals( this.eligFlagState ) ) {
+				this.states = new HashSet<>(Arrays.asList( Constants.ALL_STATE_VALUES ));
+			} else {
+				this.states = new HashSet<>();
+			}
 			BasEligState basEligState = new BasEligState();
 			basEligState.runQuery( this.eligRulesId, this.effdtStr );
 
 			try {
 				while( basEligState.queryResult.next() ) {
-					this.states.add( basEligState.queryResult.getString( "STATE" ) );
+					// if the flag was Ineligible, remove this state from the list of all states
+					if( "I".equals( this.eligFlagState ) ) {
+						this.states.remove( basEligState.queryResult.getString( "STATE" ) );
+					} else {
+					// otherwise, add this state to the eligible list
+						this.states.add( basEligState.queryResult.getString( "STATE" ) );
+					}
 				}
 			} catch( Exception e ) {
 				System.out.println( "general exception when reading BAS_ELIG_STATE rows" );
@@ -52,7 +68,7 @@ public class PSEligRule {
 
 	/* main method for testing only */
 	public static void main( String[] args ) {
-		PSEligRule elig = new PSEligRule( "2123", "12-JAN-2018" );
+		PSEligRule elig = new PSEligRule( "2124", "12-JAN-2018" );
 		System.out.println( "object:eligRulesId:" + elig.eligRulesId );
 		System.out.println( "object:effdtStr:" + elig.effdtStr );
 		System.out.println( "object:eligFlagState:" + elig.eligFlagState );
