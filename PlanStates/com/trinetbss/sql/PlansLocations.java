5cc76fe4d1eba231227e591bc68ad840bfb90a58
mikebro <Michael.Brothers@trinet.com>
2017-11-08 18:28:36 -0800

    upgraded PlansLocations query to include all BSS plan types

--- PlanStates/com/trinetbss/sql/PlansLocations.java
+++ PlanStates/com/trinetbss/sql/PlansLocations.java
@@ -21,26 +21,35 @@ public class PlansLocations {
 			Connection vDatabaseConnection = psconn.getConnection();
 
 			String sqlString =
-				  "SELECT DISTINCT PLAN_TYPE " +
-				  "              , BENEFIT_PLAN " +
-				  "              , ( SELECT BP.DESCR " +
-				  "                    FROM PS_BENEF_PLAN_TBL BP " +
-				  "                   WHERE BP.PLAN_TYPE = OP.PLAN_TYPE " +
-				  "                     AND BP.BENEFIT_PLAN = OP.BENEFIT_PLAN " +
-				  "                     AND BP.EFFDT = (SELECT MAX( EFFDT ) " +
-				  "                                       FROM PS_BENEF_PLAN_TBL B1 " +
-				  "                                      WHERE B1.PLAN_TYPE = BP.PLAN_TYPE " +
-				  "                                        AND B1.BENEFIT_PLAN = BP.BENEFIT_PLAN " +
-				  "                                        AND B1.EFFDT <= OP.EFFDT) ) " +
-				  "                   AS PLAN_NAME " +
-				  "              , LOCATION_TBL_ID " +
-				  "  FROM PS_BEN_DEFN_OPTN OP " +
-				  " WHERE BENEFIT_PROGRAM = ? " +
-				  "   AND EFFDT = TO_DATE( ?, 'DD-MON-YYYY' ) " +
-				  "   AND PLAN_TYPE = '10' " +
-				  "   AND OPTION_TYPE = 'O' " +
-				  "   AND NOT ( ELIG_RULES_ID IN ('2009', '23GC', '236Q') ) " +
-				  "   AND NOT ( LOCATION_TBL_ID = ' ' )";
+					"SELECT DISTINCT " +
+					"       OP.PLAN_TYPE " +
+					"     , OP.BENEFIT_PLAN " +
+					"     , ( SELECT BP.DESCR " +
+					"           FROM PS_BENEF_PLAN_TBL BP " +
+					"          WHERE BP.PLAN_TYPE = OP.PLAN_TYPE " +
+					"            AND BP.BENEFIT_PLAN = OP.BENEFIT_PLAN " +
+					"            AND BP.EFFDT = ( " +
+					"                SELECT MAX( EFFDT ) " +
+					"                  FROM PS_BENEF_PLAN_TBL B1 " +
+					"                 WHERE B1.PLAN_TYPE = BP.PLAN_TYPE " +
+					"                   AND B1.BENEFIT_PLAN = BP.BENEFIT_PLAN " +
+					"                   AND B1.EFFDT <= OP.EFFDT ) ) " +
+					"          AS PLAN_NAME " +
+					"     , OP.LOCATION_TBL_ID " +
+					"     , OP.ELIG_RULES_ID " +
+					"  FROM PS_BEN_DEFN_OPTN OP " +
+					" WHERE OP.BENEFIT_PROGRAM = ? " +
+					"   AND OP.EFFDT = TO_DATE( ?, 'DD-MON-YYYY' ) " +
+					"   AND OP.PLAN_TYPE IN ( '10','11','14','1D','1V','23','30','31' ) " +
+					"   AND OP.OPTION_TYPE = 'O' " +
+					"   AND NOT ( OP.ELIG_RULES_ID IN ('2009', '23GC', '236Q') ) " +
+					"   --EXCLUDE THE INTERNATIONAL PLAN " +
+					"   AND NOT ( OP.BENEFIT_PLAN = '0003M3' ) " +
+					"   --EXCLUDE MIRROR PLANS " +
+					"   AND NOT EXISTS ( " +
+					"          SELECT 'X' " +
+					"            FROM PS_T2_ACA_MIRR_TBL MIR " +
+					"           WHERE MIR.T2_MIRR_BEN_PLAN = OP.BENEFIT_PLAN ) ";
 
 			// Prepare statement from SQL string
 			PreparedStatement sqlStmt = vDatabaseConnection.prepareStatement( sqlString );
@@ -50,16 +59,7 @@ public class PlansLocations {
 			sqlStmt.setString( 2, effdtStr );
 
 			// run the query
-			queryResult = sqlStmt.executeQuery();
-
-/*       while( qResult.next() ) {
-				String planType = qResult.getString( "PLAN_TYPE" );
-			   String benefitPlan = qResult.getString( "BENEFIT_PLAN" );
-				String planName = qResult.getString( "PLAN_NAME" );
-				String geoLoc = qResult.getString( "LOCATION_TBL_ID" );
-
-				System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
-			}  */
+			this.queryResult = sqlStmt.executeQuery();
 
 		} catch( SQLException e ) {
 			System.out.println( e.toString() );
@@ -71,9 +71,7 @@ public class PlansLocations {
 	public static void main( String[] args ) {
 		System.out.println( "PlansLocations.main()" );
 		PlansLocations pbs = new PlansLocations();
-		pbs.runQuery( "108", "01-OCT-2017" );
-
-		System.out.println( "display results in caller" );
+		pbs.runQuery( "101", "01-JAN-2018" );
 
 		try {
 			while( pbs.queryResult.next() ) {
@@ -81,8 +79,9 @@ public class PlansLocations {
 				String benefitPlan = pbs.queryResult.getString( "BENEFIT_PLAN" );
 				String planName = pbs.queryResult.getString( "PLAN_NAME" );
 				String geoLoc = pbs.queryResult.getString( "LOCATION_TBL_ID" );
+				String eligRule = pbs.queryResult.getString( "ELIG_RULES_ID" );
 
-				System.out.println( "=====>" + planType + " <=> " + benefitPlan + " <=> " + planName + " <=> " + geoLoc );
+				System.out.println( "====>" + planType + ":" + benefitPlan + ":" + planName + ":" + geoLoc + ":" + eligRule );
 			}
 		} catch( SQLException e ) {
 			System.out.println( e.toString() );
