291f96b49d73c56c3cea1be9b8388d48787bc0fc
mikebro <Michael.Brothers@trinet.com>
2017-12-12 11:47:04 -0800

    added class to test various char values

---
+++ TestChar.java
@@ -0,0 +1,13 @@
+
+public class TestChar {
+
+   public static void main(String[] args) {
+
+		char c = 0;
+		System.out.println( "Set to low-value:" + c );
+
+		c = '0';
+		System.out.println( "Set to character '0':" + c );
+
+   }
+}
