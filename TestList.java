9aae3a362262808276dedc33bdf4e14483b3848b
mikebro <Michael.Brothers@trinet.com>
2018-04-05 17:02:27 -0700

    added test of List.addAll when parameter is null (caused NPE)

--- TestList.java
+++ TestList.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class TestList {
-   public static void main(String[] args) {
+	public static void main( String[] args ) {
+		main3( args );
+	}
+
+
+   public static void main1(String[] args) {
       List<String> list = new ArrayList<String>();
 
       list.add( "23" );
@@ -15,4 +21,53 @@ public class TestList {
       }
 
    }
+
+
+   public static void main2(String[] args) {
+
+      System.out.println("started email list test");
+      
+      List<String> emailList = new ArrayList<String>();
+      
+      emailList.add( "mike.bro@att.net" );
+      emailList.add( "yomikebro@gmail.com" );
+      emailList.add( "mike.bro@att.net" );  
+      emailList.add( "michael.brothers@trinet.com" );
+
+      
+      for( String s : emailList ) {
+         System.out.println( "::" + s + "::" );
+         // createEmailRequest
+      }
+   
+      System.out.println( "toString():" + emailList.toString() + ":" );
+      System.out.println( "List contains " + emailList.size() + " elements." );
+   }
+
+
+   public static void main3(String[] args) {
+		// test when parameter passed to addAll method is null
+      System.out.println("started null parm test test");
+      
+      List<String> emailList = new ArrayList<String>();
+		Collection<String> someColl = null;
+      
+      emailList.add( "mike.bro@att.net" );
+      emailList.add( "yomikebro@gmail.com" );
+      emailList.add( "mike.bro@att.net" );  
+      emailList.add( "michael.brothers@trinet.com" );
+
+      emailList.addAll( someColl );
+/*  caused:
+Exception in thread "main" java.lang.NullPointerException
+        at java.util.ArrayList.addAll(ArrayList.java:581)
+        at TestList.main3(TestList.java:60)
+        at TestList.main(TestList.java:7)
+*/   
+
+      System.out.println( "toString():" + emailList.toString() + ":" );
+      System.out.println( "List contains " + emailList.size() + " elements." );
+   }
+
+
 }
