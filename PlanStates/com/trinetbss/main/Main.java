5e1cca0521c36ac6b48fe75a53502a27eac21fb9
mikebro <Michael.Brothers@trinet.com>
2018-08-30 10:58:24 -0700

    Updated settings to generate SOI_SM_2019 TEST data

--- PlanStates/com/trinetbss/main/Main.java
+++ PlanStates/com/trinetbss/main/Main.java
@@ -27,9 +27,9 @@ public class Main {
 	public static void main( String[] args ) {
       System.out.println( "Main.main()" );
 
-		int realmYearId = 16;
-		String benefitProgram = "112";
-		String effdtStr = "01-JAN-2019";
+		int realmYearId = 18;
+		String benefitProgram = "114";
+		String effdtStr = "01-APR-2019";
 
 		// prepare output object
 		BSSTableData bss = new BSSTableData();
