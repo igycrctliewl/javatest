3ec5f8d6af9fb5eb9276dd6160dc0a8593f97886
mikebro <Michael.Brothers@trinet.com>
2018-09-07 13:29:28 -0700

    reused TestList creating a list of zip codes from zip code range data

--- TestList.java
+++ TestList.java
@@ -1,18 +1,52 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class TestList {
-   public static void main(String[] args) {
-      List<String> list = new ArrayList<String>();
+	private Set<Integer> locations;
 
-      list.add( "23" );
-      list.add( "30" );
-      list.add( "31" );
-      list.add( "A3" );
+	public TestList() {
+		locations = new HashSet<Integer>();
+	}
 
-      for( String planType : list ) {
-         System.out.println( "===>  " + planType );
-      }
+	public int getSetSize() {
+		return locations.size();
+	}
 
-   }
+	public void addRange( String from, String to ) {
+		int intFrom = Integer.parseInt( from );
+		int intTo;
+		if( to.length() > 5 )
+			intTo = Integer.parseInt( to.substring( 0, 5 ) );
+		else
+			intTo = Integer.parseInt( to );
+
+		for( int k = intFrom; k <= intTo; k++ ) {
+			System.out.println( "Adding " + k + " to set." );
+			locations.add( k );
+		}
+	}
+
+	public boolean isIncluded( Integer zip ) {
+		return locations.contains( zip );
+	}
+
+
+
+	public static void main(String[] args) {
+
+		TestList tl = new TestList();
+
+		tl.addRange( "93673", "936739999" );
+		tl.addRange( "93675", "936759999" );
+		tl.addRange( "93675", "93675" );
+		tl.addRange( "93701", "937129999" );
+		tl.addRange( "93714", "937189999" );
+
+		System.out.println( tl );
+		System.out.println( tl.getSetSize() );
+
+		System.out.println( tl.isIncluded( 94044 ) );
+		System.out.println( tl.isIncluded( 93716 ) );
+
+	}
 }
